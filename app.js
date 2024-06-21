45cdb5fd301e43508db0161a5e7652a941203f71
Jean Dale Joseph <silicon.logic.tech@gmail.com>
2024-06-21 19:36:08 +0800

    called the elementId in my js file

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 function update(){
     let bill = document.getElementById("yourBill").value;
+    let tipPercent = document.getElementById("tipInput").value;
+    let split = document.getElementById("splitInput").value;
     console.log(bill);
 }
