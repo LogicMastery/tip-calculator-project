1e386b3cb2bcda059a99280e8af04b2d6ab2a94a
Jean Dale Joseph <silicon.logic.tech@gmail.com>
2024-06-27 14:27:42 +0800

    added calculation for the tip

--- app.js
+++ app.js
@@ -2,7 +2,8 @@ function update(){
     let bill = document.getElementById("yourBill").value;
     let tipPercent = document.getElementById("tipInput").value;
     let split = document.getElementById("splitInput").value;
-    console.log({bill, tipPercent, split});
+    
+    let tipValue = bill * (tipPercent / 100);
 }
 
 let container = document.getElementById("container");
